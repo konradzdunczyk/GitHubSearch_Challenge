8a7099d05b7de23b5d9a205c6ab37d36ac2b85e7
Konrad Zdunczyk <konrad.zdunczyk@gmail.com>
2021-10-13 14:58:33 +0200

    Init project

---
+++ GitHubSearch_Challenge/GitHubSearch_Challenge/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  GitHubSearch_Challenge
+//
+//  Created by Zduńczyk Konrad on 13/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
